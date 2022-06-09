c71dfbc017d0aa6efb2fb29b371c583c58a143ba
david <david@DESKTOP-499U3MH.station>
2022-06-09 15:37:03 +0200

    Modifica Davide

---
+++ src/main/java/com/acme/git/tondini/Davide.java
@@ -0,0 +1,13 @@
+package com.acme.git.tondini;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Davide {
+	private String nome;
+	private String cognome;
+}
