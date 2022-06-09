23be7986b5071159c1107c1d0a3d9221825e9932
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-09 15:21:35 +0200

    first commit

---
+++ src/main/java/com/acme/git/ProgettoTestGruppo1Application.java
@@ -0,0 +1,13 @@
+package com.acme.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTestGruppo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoTestGruppo1Application.class, args);
+	}
+
+}
