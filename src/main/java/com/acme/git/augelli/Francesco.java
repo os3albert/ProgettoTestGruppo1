73797fc397103e5a07888d18fd8a87f0aee8ff18
fraug <fraug@192.168.178.116>
2022-06-09 15:32:30 +0200

    Variazione da Francesco

---
+++ src/main/java/com/acme/git/augelli/Francesco.java
@@ -0,0 +1,16 @@
+package com.acme.git.augelli;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Francesco {
+	
+	private String nome;
+	private String cognome;
+
+}
